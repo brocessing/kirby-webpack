35156c4e67895ba142e97659e8ac6d7af9834eca
RNO <arnaud.juracek@gmail.com>
2017-08-30 16:19:27 +0200

    Clear modules list

--- kirby.config.js
+++ kirby.config.js
@@ -2,12 +2,8 @@ module.exports = {
   modules: {
     core: 'https://github.com/getkirby/kirby.git',
     panel: 'https://github.com/getkirby/panel.git',
-    fields: {
-      'markdown': 'https://github.com/JonasDoebertin/kirby-visual-markdown.git'
-    },
+    fields: {},
     plugins: {},
-    widgets: {
-      'kirby-backup-widget': 'https://github.com/arnaudjuracek/kirby-backup-widget.git'
-    }
+    widgets: {}
   }
 }
